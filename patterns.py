fda53a8435909163d5acbc23c51df36029c6f142
zacharesmer <zachresmer@gmail.com>
2022-01-18 16:30:47 -0500

    change rainbow patterns to look nicer

--- patterns.py
+++ patterns.py
@@ -10,7 +10,7 @@ num_pixels = 100
 pixels=neopixel.NeoPixel(board.D21, num_pixels, auto_write=False)
 
 dim = .95
-max_brightness = 255
+max_brightness = 50
 
 h=0
 
@@ -42,36 +42,37 @@ def solid_chase(r, g, b):
 
 def rainbow_chase(r, g, b):
     s = 1.0
-    v = 1.0
+    v = max_brightness/255
     # this should probably be changed 
     global h
-    rgb = colorsys.hsv_to_rgb(h/255, s, v)
+    rgb = colorsys.hsv_to_rgb(h, s, v)
     for j in range(num_pixels):
-        if h > 255:
-            h = 0
-        h += 2
         pixels[j] = ((math.floor(rgb[0]*255)), (math.floor(rgb[1]*255)), (math.floor(rgb[2]*255)))
         for i in range(num_pixels):
             pixels[i] = (math.floor(dim * pixels[i][0]),  math.floor(dim * pixels[i][1]), math.floor(dim * pixels[i][2]))
+            if h > 1:
+                h = 0
+            h += .0003
+            rgb = colorsys.hsv_to_rgb(h, s, v)
         pixels.show()
 
 
 def rainbow_cycle(r, g, b):
     s = 1.0
-    v = 1.0
+    v = max_brightness/255
     # this should probably be changed
     global h
-    if h>255:
+    if h>1:
         h=0
-    h += 1
-    rgb = colorsys.hsv_to_rgb(h/255, s, v)
+    h += .0001
+    rgb = colorsys.hsv_to_rgb(h, s, v)
     pixels.fill(((math.floor(rgb[0]*255)), (math.floor(rgb[1]*255)), (math.floor(rgb[2]*255))))
-    time.sleep(.05)
+    time.sleep(.001)
     pixels.show()
 
 def rainbow_fill(r, g, b):
     s = 1.0
-    v = 1.0
+    v = max_brightness/255
     i = 0
     while(i<num_pixels):
         for h in range(0, 255, 2):
