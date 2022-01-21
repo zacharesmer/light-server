f2611d4c66f6c90bf29b02b64a6460256ca98c7a
zacharesmer <zachresmer@gmail.com>
2022-01-21 13:28:11 -0500

    refactor into a class

---
+++ lights.py
@@ -0,0 +1,130 @@
+import board
+import neopixel
+import random
+import math
+import threading
+import time
+import colorsys
+import datetime
+
+class Lights:
+    '''
+    A class to hold patterns of lights and a NeoPixel instance. 
+    Also has a threading Event to manage the lights
+    '''
+    def __init__(self, num_pixels, max_brightness):
+        self.num_pixels = num_pixels
+        self.max_brightness = max_brightness
+
+        # defaults to False, waits to become True
+        self.stop = threading.Event()
+        # the lights should initially be stopped until someone starts a pattern
+        self.stop.set()
+        # this will hold the thread the lights are currently running in
+        self.thread = None
+
+        self.pixels=neopixel.NeoPixel(board.D21, num_pixels, auto_write=False)
+        self.available_patterns = ["random_chase", "solid_chase", "rainbow_chase", "rainbow_cycle", "fill", "rainbow_fill", "rainbow_scroll", "clock"]
+
+        self.dim = .95
+        self.hue=0
+
+
+    def pattern(self, p, r, g, b):
+        while(not self.stop):
+            eval(p+"(self, r, g, b)")
+        self.blank()
+
+    def fill(self, r, g, b):
+        self.pixels.fill((r, b, g))
+        self.pixels.show()
+
+    def blank(self):
+        self.pixels.fill((0,0,0))
+        self.pixels.show()
+
+    def random_chase(self, r, g, b):
+        for j in range(self.num_pixels):
+            self.self.pixels[j] = (random.randint(0, max_brightness), random.randint(0, max_brightness),random.randint(0, max_brightness))
+            for i in range(self.num_pixels):
+                self.self.pixels[i] = (math.floor(self.dim * self.pixels[i][0]),  math.floor(self.dim * self.pixels[i][1]), math.floor(self.dim * self.pixels[i][2]))
+            self.pixels.show()
+
+    def solid_chase(self, r, g, b):
+        for j in range(self.num_pixels):
+            self.pixels[j] = (r, b, g)
+            for i in range(self.num_pixels):
+                self.pixels[i] = (math.floor(self.dim * self.pixels[i][0]),  math.floor(self.dim * self.pixels[i][1]), math.floor(self.dim * self.pixels[i][2]))
+            self.pixels.show()
+
+    def rainbow_chase(self, r, g, b):
+        sat = 1.0
+        val = max_brightness/255
+        rgb = colorsys.hsv_to_rgb(self.hue, s, v)
+        for j in range(self.num_pixels):
+            self.pixels[j] = ((math.floor(rgb[0]*255)), (math.floor(rgb[1]*255)), (math.floor(rgb[2]*255)))
+            for i in range(self.num_pixels):
+                self.pixels[i] = (math.floor(self.dim * self.pixels[i][0]),  
+                        math.floor(self.dim * self.pixels[i][1]), math.floor(self.dim * self.pixels[i][2]))
+                if self.hue > 1:
+                    self.hue = 0
+                self.hue += .0003
+                rgb = colorsys.hsv_to_rgb(self.hue, s, v)
+            self.pixels.show()
+
+
+    def rainbow_cycle(self, r, g, b):
+        sat = 1.0
+        val = self.max_brightness/255
+        if self.hue > 1:
+            self.hue =0
+        self.hue += .0001
+        rgb = colorsys.hsv_to_rgb(self.hue, sat, val)
+        self.pixels.fill(((math.floor(rgb[0]*255)), (math.floor(rgb[1]*255)), (math.floor(rgb[2]*255))))
+        time.sleep(.001)
+        self.pixels.show()
+
+    def rainbow_fill(self, r, g, b):
+        s = 1.0
+        v = self.max_brightness/255
+        i = 0
+        while(i<self.num_pixels):
+            for h in range(0, 255, 2):
+                rgb = colorsys.hsv_to_rgb(h/255, s, v)
+                self.pixels[i] = ((math.floor(rgb[0]*255)), (math.floor(rgb[1]*255)), (math.floor(rgb[2]*255)))
+                i += 1
+                if i >= self.num_pixels:
+                    break
+        self.pixels.show()
+
+    def rainbow_scroll(self, r, g, b):
+        s = 1.0
+        v = self.max_brightness/255
+        h_offset = round(time.time()*25)
+
+        for pixel_ind in range(self.num_pixels):
+            rgb = colorsys.hsv_to_rgb(((h_offset + pixel_ind) % 255)/255, s, v)
+            self.pixels[pixel_ind] = ((math.floor(rgb[0]*255)), (math.floor(rgb[1]*255)), (math.floor(rgb[2]*255)))
+        self.pixels.show()
+         
+    def clock(self, r, g, b):
+        leds_on = math.floor(self.timedelta_percentage(datetime.datetime.now()) * self.num_pixels)
+        led_blink_index = math.ceil(self.timedelta_percentage(datetime.datetime.now()) * self.num_pixels)
+
+        blink_status_on = round(time.time()) % 2 == 1
+        
+        for pixel_ind in range(leds_on):
+            self.pixels[pixel_ind] = (r,b,g)
+        if blink_status_on:
+            self.pixels[led_blink_index-1] = (r,b,g)
+        else:
+            self.pixels[led_blink_index-1] = (0,0,0)
+        self.pixels.show()
+
+    def timedelta_percentage(self, input_datetime):
+        d = input_datetime - datetime.datetime.combine(input_datetime.date(), datetime.time())
+        return d.total_seconds() / 86400.0
+
+
+if __name__ == "__main__":
+    patterns = Lights(100, 255)
--- server.py
+++ server.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template
 import threading
-import patterns
+import lights
 from markupsafe import escape
 
 app = Flask(__name__)
 
-run_lights = None
-
 @app.route("/")
 def hello_world():
-    return(render_template("home.html", available_patterns=patterns.available_patterns))
+    return render_template("home.html", available_patterns=lights.available_patterns)
 
 @app.route("/start")
 def pick_pattern():
@@ -18,36 +16,44 @@ def pick_pattern():
 @app.route("/start/<p>/", defaults={"r":50, "g":50, "b":50})
 @app.route("/start/<p>/<int:r>+<int:g>+<int:b>")
 def start_pattern(p, r, g, b):
-    global run_lights 
-    # stop the current pattern if it exists
-    # TODO: the old pattern is not always stopping before the new pattern
-    if run_lights is not None:
+    # if lights are not stopped, stop them
+    if not lights.stop:
         stop_pattern()
     # check if pattern is valid
-    if p not in patterns.available_patterns:
+    if p not in lights.available_patterns:
         return("not today >:)")
     # Verify that the old pattern has actually stopped
     # If it has, run the new pattern in a thread
-    if run_lights is None:
-        run_lights = threading.Thread(target=patterns.pattern, args=[p, r, g, b])
-        run_lights.start()
+    if lights.thread is None:
+        lights.thread = threading.Thread(target=lights.pattern, args=[lights, p, r, g, b])
+        lights.thread.start()
+        lights.stop.clear()
         return(f"Started {escape(p)}")
     return("old pattern not stopped successfully")
 
 @app.route("/stop")
 def stop_pattern():
-    patterns.blank()
+    lights.blank()
     try:
-        global run_lights
-        run_lights.do_run = False
-        run_lights.join()
-        run_lights = None
-        patterns.blank()
+        lights.stop.set()
+        # join the thread to wait for it to finish
+        lights.thread.join()
+        # now there's no more thread so update the variable to point to None
+        lights.thread = None
+        # turn off all the lights
+        lights.blank()
         return("Stopped!")
     except Exception as e:
+        # There might be an exception if there is no thread active
         print(e)
-        patterns.blank()
+        lights.blank()
         return("Nothing to stop.")
 
+@app.route("/brightness/<int:brightness>")
+def set_brightness(brightness):
+    if (brightness < 256 and brightness >=0):
+        lights.max_brightness = brightness
+
 if __name__ == "__main__":
+    lights = lights.Lights(num_pixels=100, max_brightness=255)
     app.run(host="0.0.0.0", port=5000, debug=True)
